31c011a786d7fd94eab5927a24c81e5eaf76b2cb
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2024-12-13 14:49:19 -0500

    PlanetOM updates

---
+++ Projects/PlanetOM/script/script.js
@@ -0,0 +1,6 @@
+const typinText = document.getElementById("hmm");
+const struggleCard = document.getElementById("struggle-card");
+
+typinText.addEventListener('click', () => {
+    window.location.href = "#autot-card";
+});
