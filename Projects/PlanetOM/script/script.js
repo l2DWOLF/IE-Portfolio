bb0bd3dd11e2b649702db813c534204e5d9aba98
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2024-12-13 18:23:52 -0500

    mobifix

--- Projects/PlanetOM/script/script.js
+++ Projects/PlanetOM/script/script.js
@@ -1,5 +1,4 @@
 const typinText = document.getElementById("hmm");
-const struggleCard = document.getElementById("struggle-card");
 
 typinText.addEventListener('click', () => {
     window.location.href = "#autot-card";
