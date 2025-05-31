7b44bf90bebf4be96eba51203ae335977d646c15
IE <123218119+l2DWOLF@users.noreply.github.com>
2025-05-31 22:08:21 +0300

    Update app.js

--- Projects/JavaScript/Text-to-Voice/js/app.js
+++ Projects/JavaScript/Text-to-Voice/js/app.js
@@ -98,7 +98,7 @@ resetBtn.addEventListener('click', () => {
 
 
 quoteBtn.addEventListener('click', () => {
-    await getQuote();
+    getQuote(); // fix
     textInput.value = `"..${theQuote}..."`;
     textInput.value += ` \n \nAuthor: "${theAuthor} " `;
     
